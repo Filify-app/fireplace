cfa81a25a976af9e2e30b2bd7b69b154e70727cb
Adrian Borup <adrian@borup.com>
2022-09-20 15:56:05 +0200

    Implement, document, and test set_document and set_document_merge

--- src/firestore/client.rs
+++ src/firestore/client.rs
@@ -1,7 +1,9 @@
+use std::fmt::Display;
+
 use anyhow::{anyhow, Context};
 use firestore_grpc::tonic;
 use firestore_grpc::v1::firestore_client::FirestoreClient as GrpcFirestoreClient;
-use firestore_grpc::v1::{CreateDocumentRequest, DocumentMask};
+use firestore_grpc::v1::{CreateDocumentRequest, DocumentMask, UpdateDocumentRequest};
 use firestore_grpc::{
     tonic::{
         codegen::InterceptedService,
@@ -113,10 +115,8 @@ impl FirestoreClient {
         &mut self,
         doc_ref: &DocumentReference,
     ) -> Result<Option<T>, FirebaseError> {
-        let name = format!("{}/{}", self.root_resource_path, doc_ref);
-
         let request = GetDocumentRequest {
-            name,
+            name: self.get_name_with(doc_ref),
             mask: None,
             consistency_selector: None,
         };
@@ -181,7 +181,7 @@ impl FirestoreClient {
     /// ));
     /// # }
     /// ```
-    pub async fn create_document<'de, T: Serialize>(
+    pub async fn create_document<T: Serialize>(
         &mut self,
         collection_ref: &CollectionReference,
         document_id: Option<String>,
@@ -220,5 +220,200 @@ impl FirestoreClient {
             ),
             Err(err) => Err(anyhow!(err).into()),
         }
+    }
+
+    /// Sets a document at the given document reference. If it doesn't already,
+    /// exist, it is created - and if it does exist already, it is overwritten.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # #[tokio::main]
+    /// # async fn main() {
+    /// # use firebase_admin_rs::firestore::{client::FirestoreClient, reference::{CollectionReference, DocumentReference}};
+    /// # let mut client = FirestoreClient::initialise(
+    /// #     &std::env::var("PROJECT_ID").unwrap(),
+    /// #     &std::env::var("TOKEN").unwrap(),
+    /// # )
+    /// # .await
+    /// # .unwrap();
+    /// #
+    /// let doc_ref = CollectionReference::new("greetings").doc("some-doc-id-to-set");
+    /// let doc = serde_json::json!({ "message": "Hello, world!".to_string() });
+    ///
+    /// // We can upsert the document in the database
+    /// client.set_document(&doc_ref, &doc).await.unwrap();
+    ///
+    /// // We can write to the same document reference again, and it will overwrite
+    /// // the existing value document
+    /// client.set_document(&doc_ref, &doc).await.unwrap();
+    /// # }
+    /// ```
+    pub async fn set_document<T: Serialize>(
+        &mut self,
+        doc_ref: &DocumentReference,
+        document: &T,
+    ) -> Result<(), FirebaseError> {
+        let name = self.get_name_with(doc_ref);
+        let doc = serialize_to_document(document, Some(name), None, None)?;
+
+        let request = UpdateDocumentRequest {
+            document: Some(doc),
+            update_mask: None,
+            mask: Some(DocumentMask {
+                field_paths: vec![],
+            }),
+            current_document: None,
+        };
+
+        self.client
+            .update_document(request)
+            .await
+            .map_err(|err| anyhow!(err))?;
+
+        Ok(())
+    }
+
+    /// Similar to [`set_document`](Self::set_document) but only upserts the
+    /// fields specified in the `fields` argument.
+    ///
+    /// Generic type parameters: `I` for the input type that's to be serialized
+    /// and `O` for the returned (full) document that should be deserialized.
+    ///
+    /// # Field selectors
+    ///
+    /// A simple field name contains only characters `a` to `z`, `A` to `Z`, `0`
+    /// to `9`, or `_`, and must not start with `0` to `9`. For example,
+    /// `foo_bar_17`.
+    ///
+    /// Field names matching the regular expression `__.*__` are reserved.
+    /// Reserved field names are forbidden except in certain documented
+    /// contexts. The map keys, represented as UTF-8, must not exceed 1,500
+    /// bytes and cannot be empty.
+    ///
+    /// Field paths may be used in other contexts to refer to structured fields
+    /// defined here. For map-like values, the field path is represented by the
+    /// simple or quoted field names of the containing fields, delimited by `.`.
+    /// For example, the field `"foo": { "x&y": "hello" }` would be represented
+    /// by the field path `foo.x&y`.
+    ///
+    /// The above is a slightly modified description from the [Firestore API reference](https://firebase.google.com/docs/firestore/reference/rpc/google.firestore.v1#document).
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # #[tokio::main]
+    /// # async fn main() {
+    /// # use firebase_admin_rs::firestore::{client::FirestoreClient, reference::{CollectionReference, DocumentReference}};
+    /// # use serde::{Deserialize, Serialize};
+    /// # let mut client = FirestoreClient::initialise(
+    /// #     &std::env::var("PROJECT_ID").unwrap(),
+    /// #     &std::env::var("TOKEN").unwrap(),
+    /// # )
+    /// # .await
+    /// # .unwrap();
+    /// #
+    /// #[derive(Debug, Serialize, Deserialize, PartialEq)]
+    /// struct TestType {
+    ///     label: String,
+    ///     nested: NestedItem,
+    /// }
+    ///
+    /// #[derive(Debug, Serialize, Deserialize, PartialEq)]
+    /// #[serde(rename_all = "camelCase")]
+    /// struct NestedItem {
+    ///     field_a: String,
+    ///     field_b: String,
+    /// }
+    ///
+    /// // First, we set a document in the database
+    /// let doc_ref = CollectionReference::new("greetings").doc("some-doc-id-to-set-merge");
+    /// client
+    ///     .set_document(
+    ///         &doc_ref,
+    ///         &TestType {
+    ///             label: "Hello".to_string(),
+    ///             nested: NestedItem {
+    ///                 field_a: "A".to_string(),
+    ///                 field_b: "B".to_string(),
+    ///             },
+    ///         },
+    ///     )
+    ///     .await
+    ///     .unwrap();
+    ///
+    /// // Then we can update some fields of a document in the database. For
+    /// // example, we can specify a top-level field ("label") or a nested field
+    /// // ("nested.fieldA").
+    /// let updated_doc: TestType = client
+    ///     .set_document_merge(
+    ///         &doc_ref,
+    ///         &TestType {
+    ///             label: "World".to_string(),
+    ///             nested: NestedItem {
+    ///                 field_a: "C".to_string(),
+    ///                 field_b: "D".to_string(),
+    ///             },
+    ///         },
+    ///         &["label", "nested.fieldB"],
+    ///     )
+    ///     .await
+    ///     .unwrap();
+    ///
+    /// // Only the specified fields are updated. Despite `nested.field_a` having a
+    /// // new value in the update, the value in the database is not changed.
+    /// assert_eq!(
+    ///     updated_doc,
+    ///     TestType {
+    ///         label: "World".to_string(),
+    ///         nested: NestedItem {
+    ///             field_a: "A".to_string(), // Notice this field did not change
+    ///             field_b: "D".to_string(),
+    ///         },
+    ///     }
+    /// );
+    /// # }
+    /// ```
+    pub async fn set_document_merge<'de, I: Serialize, O: Deserialize<'de>>(
+        &mut self,
+        doc_ref: &DocumentReference,
+        document: &I,
+        // In reality we need a `Vec<String>`, but in by far most of the use-
+        // cases, the user will be hard-coding the field names, so this makes
+        // it much easier to just do that.
+        fields: &[&str],
+    ) -> Result<O, FirebaseError> {
+        let name = self.get_name_with(doc_ref);
+        let doc = serialize_to_document(document, Some(name), None, None)?;
+
+        let update_mask = if fields.is_empty() {
+            None
+        } else {
+            Some(DocumentMask {
+                field_paths: fields.iter().map(|s| s.to_string()).collect(),
+            })
+        };
+
+        let request = UpdateDocumentRequest {
+            document: Some(doc),
+            update_mask,
+            mask: None,
+            current_document: None,
+        };
+
+        let res = self
+            .client
+            .update_document(request)
+            .await
+            .map_err(|err| anyhow!(err))?;
+
+        let doc = res.into_inner();
+        let deserialized = deserialize_firestore_document::<O>(doc)?;
+
+        Ok(deserialized)
+    }
+
+    fn get_name_with(&self, item: impl Display) -> String {
+        format!("{}/{}", self.root_resource_path, item)
     }
 }
