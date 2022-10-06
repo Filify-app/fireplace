17c667fc26a24d4f0985eeda03e57b9f372777fd
Adrian Borup <adrian@borup.com>
2022-10-06 17:05:33 +0200

    Add docs and tests for decoding token

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -124,6 +124,52 @@ impl FirebaseAuthClient {
         }
     }
 
+    /// Decodes an ID token and returns its claims. Only succeeds if the token
+    /// is valid. The token is valid if it:
+    ///
+    /// - Is not expired
+    /// - Is issued for this Firebase project
+    /// - Has a valid digital signature from Google
+    ///
+    /// The [Firebase API docs] list further requirements.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), fireplace::error::FirebaseError> {
+    /// # use ulid::Ulid;
+    /// # let mut auth_client = fireplace::auth::test_helpers::initialise()?;
+    /// // Create some user so we can get a valid ID token
+    /// let signed_up_user = auth_client
+    ///     .sign_up_with_email_and_password(format!("{}@example.com", Ulid::new()), Ulid::new())
+    ///     .await?;
+    ///
+    /// // Decode the ID token. If we get Ok back, we know it's valid and the
+    /// // user is authenticated.
+    /// let decoded_token = auth_client.decode_id_token(signed_up_user.id_token).await?;
+    ///
+    /// assert_eq!(decoded_token.user_id, signed_up_user.user_uid);
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// ```
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), fireplace::error::FirebaseError> {
+    /// # use ulid::Ulid;
+    /// # let mut auth_client = fireplace::auth::test_helpers::initialise()?;
+    /// // Some invalid ID token. It is expired, and it might be issued for a
+    /// // different Firebase project.
+    /// let id_token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjU4NWI5MGI1OWM2YjM2ZDNjOTBkZjBlOTEwNDQ1M2U2MmY4ODdmNzciLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcnVzdC1hZG1pbi1zZGstdGVzdCIsImF1ZCI6InJ1c3QtYWRtaW4tc2RrLXRlc3QiLCJhdXRoX3RpbWUiOjE2NjQ5OTUwNjcsInVzZXJfaWQiOiJIRnRxZ0NQc0hTTTF5SngwUnVaY0ZXbVQ5TEMzIiwic3ViIjoiSEZ0cWdDUHNIU00xeUp4MFJ1WmNGV21UOUxDMyIsImlhdCI6MTY2NDk5NTA2NywiZXhwIjoxNjY0OTk4NjY3LCJlbWFpbCI6ImZzYWZhQHRlc3RwLmFwcCIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJmc2FmYUB0ZXN0cC5hcHAiXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.ImphBsbuXJOMKyZF21YIK0PQ4ZFwPDDfJ56wW1cJkKBUhGUICW9zNv2WgCuZ03XdfexYcGabUjetruOQBx9c9eSJsPZQdAblNYk9vcBbmpaxya55HNkSbp2ZfX5S_ReUSekjiGsd53qfRLOTHxu4m-LGddE2_lfz6Mx2IAf9ij6JjU-uc5w5klmT3OAUkxUBpPyAcocwHU0WqXuOYDBo-WRL8hC2CTgQ8o0Mo-wHBsIZ_IU_SkIHG7xl2oq91Gm7q227KX7j5LnNaOgM3GuCOajPzzyKzTKAcX2pCKlkyR1bQHuefzuyPF_RME0jroOuHZm031uW_v4rnMWO3HtmDw";
+    /// let decode_result = auth_client.decode_id_token(id_token).await;
+    ///
+    /// assert!(decode_result.is_err());
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// [Firebase API docs]: https://firebase.google.com/docs/auth/admin/verify-id-tokens#verify_id_tokens_using_a_third-party_jwt_library
     #[tracing::instrument(name = "Decode ID token", skip(self, token))]
     pub async fn decode_id_token(
         &mut self,
