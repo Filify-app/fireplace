cf908c8575861093031067c334318e14462e12e0
Adrian Borup <adrian@borup.com>
2023-11-28 13:48:33 +0100

    Add documentation and doctests

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@
 //!
 //! Fireplace is a client for Firebase that seeks to provide a user-friendly
 //! interface to interact with Firestore, Firebase Auth, and similar.
+//!
+//! ## Firestore usage
+//!
+//! See the [`firestore`] module for more information.
 
 pub mod auth;
 pub mod error;
